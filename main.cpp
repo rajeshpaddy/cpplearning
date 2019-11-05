358b6ccc6984e2793cf027106d62528cd2d5645d
Rajesh Padmanabhan <anirajesh@msn.com>
2019-11-05 08:49:26 +0000

    working version and locally debuggable in vs code on mac

--- main.cpp
+++ main.cpp
@@ -1,4 +1,3 @@
-
 #include "learn01.h"
 #include <iostream>
 #include <vector>
@@ -8,33 +7,31 @@
 using std::vector;
 using std::cout;
 using std::endl;
-int main(int argc, char* argv[]){
+int main(int argc, char * argv[]){
 
-    learn01 l;
-    vector<string> in;
     std::queue<string> q ;
     std::stack<string> s;
-    in.push_back("Rajesh");
-    in.push_back("Padmanabhan");
-    cout << "arguments size:" << sizeof(argv) << ":" << sysconf(_SC_ARG_MAX) <<endl;
-    for (uint i=1;i<sizeof(argv) ;i++)
+    
+    cout << "arguments size  :" << sizeof(argv) << ":" << 72 <<endl;
+for (int i=1;i<argc ;i++)
     {
         cout << argv[i] <<std::endl;    
+
         q.push(argv[i]);
         s.push(argv[i]);
     }
     
-    for (uint i=1;i<sizeof(argv) ;i++)
+    for (int i=1;i<argc ;i++)
     {
         if (!q.empty())
         {
-            cout << "Queue:" << q.front() <<std::endl;    
+            cout << "Queue :" << q.front() <<std::endl;    
             q.pop();
         }
         
     }
     
-    for (uint i=1;i<sizeof(argv) ;i++)
+    for (int i=1;i<argc ;i++)
     {
         if(!s.empty())
         {
